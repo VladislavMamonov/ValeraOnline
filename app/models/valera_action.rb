113a069c9fc221febb09367f65b30b48862eacb6
Nichidanil <nichi.par@mail.ru>
2022-01-22 03:32:35 +0700

    Add valera_action

--- app/models/valera_action.rb
+++ app/models/valera_action.rb
@@ -1,2 +1,11 @@
 class ValeraAction < ApplicationRecord
+  belongs_to :user
+  validates :name, presence: true, uniqueness: true
+  validates :health, presence: true
+  validates :alcohol, presence: true
+  validates :happy, presence: true
+  validates :tired, presence: true
+  validates :money, presence: true
+  has_many :conditions, dependent: :destroy
+  accepts_nested_attributes_for :conditions
 end
